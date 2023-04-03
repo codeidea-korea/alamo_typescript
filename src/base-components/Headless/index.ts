19da0e03b3d0f90c8c6a87782492e4d6d598323a
rubin90 <rubin9008@gmail.com>
2023-04-03 11:47:38 +0900

    파일 재 업로드

--- src/base-components/Headless/index.ts
+++
@@ -1,8 +0,0 @@
-import Dialog from './Dialog'
-import Disclosure from './Disclosure'
-import Menu from './Menu'
-import Popover from './Popover'
-import Slideover from './Slideover'
-import Tab from './Tab'
-
-export { Menu, Popover, Tab, Dialog, Disclosure, Slideover }
